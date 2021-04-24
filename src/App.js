a59569d126d520b5e14d0c3e9ac7b5a14ca519ed
tugceozgen <tugce.ozgen@demirorenteknoloji.com>
2021-04-24 20:15:44 +0300

    swiper added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {Swiper, SwiperSlide} from "swiper/react";
+import SwiperCore, {
+    Navigation,
+    Pagination,
+    Autoplay,
+    Virtual
+} from "swiper/core";
+import "swiper/swiper-bundle.css";
+
+SwiperCore.use([Navigation, Pagination, Autoplay, Virtual]);
 
 function App() {
+    const slides = [10,11,12,13,14,15]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            navigation
         >
-          Learn React
-        </a>
-      </header>
+            {slides.map((item) => {
+                return(
+                    <SwiperSlide key={item}>
+                        <img className="swiper-img" src={"https://picsum.photos/id/" + item + "/550/378"}/>
+                        <div className="swiper-title">Lorem ipsum dolor sit amet, consec tetur adipisicing elit.</div>
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
     </div>
   );
 }
